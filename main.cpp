4055983a2915053d240bacc32d5a4b98f760fd0c
Juan Diego Castro Padilla <79115974+ByJuanDiego@users.noreply.github.com>
2022-11-11 00:10:09 -0500

    gui add

--- main.cpp
+++ main.cpp
@@ -1,30 +1,37 @@
 #include "BlockChain.h"
+#include "ui/Gui.h"
 
 int main(int argc, char const *argv[])
 {
-    BlockChain bch;
+    auto* gui = new Gui;
+    if (gui->initialize()) {
+        gui->runLoop();
+    }
+    delete gui;
 
-    bch.createUser("fernando", "6077");
-    bch.createUser("carlos", "1919");
-    bch.createUser("alexis", "2020");
-
-    bch.setTx("fernando", "6077", "carlos", 200, "1657748880");
-    bch.setTx("fernando", "6077", "alexis", 2200, "1656620824");
-    bch.setTx("fernando", "6077", "juan", 500, "1645827356");
-    bch.setTx("fernando", "6077", "luis", 870, "1645827359");
-
-    bch.viewMyBlock("fernando", "6077");
-    std::cout << std::endl;
-    bch.viewMyBlock("carlos", "1919");
-    std::cout << std::endl;
-    bch.viewMyBlock("alexis", "2020");
-    std::cout << std::endl;
-
-    std::cout << std::endl;
-    bch.viewMyBlockAmount("fernando", "6077");
-
-    std::cout << std::endl;
-    bch.viewMyBlockDate("fernando", "6077");
+//    BlockChain bch;
+//
+//    bch.createUser("fernando", "6077");
+//    bch.createUser("carlos", "1919");
+//    bch.createUser("alexis", "2020");
+//
+//    bch.setTx("fernando", "6077", "carlos", 200, "1657748880");
+//    bch.setTx("fernando", "6077", "alexis", 2200, "1656620824");
+//    bch.setTx("fernando", "6077", "juan", 500, "1645827356");
+//    bch.setTx("fernando", "6077", "luis", 870, "1645827359");
+//
+//    bch.viewMyBlock("fernando", "6077");
+//    std::cout << std::endl;
+//    bch.viewMyBlock("carlos", "1919");
+//    std::cout << std::endl;
+//    bch.viewMyBlock("alexis", "2020");
+//    std::cout << std::endl;
+//
+//    std::cout << std::endl;
+//    bch.viewMyBlockAmount("fernando", "6077");
+//
+//    std::cout << std::endl;
+//    bch.viewMyBlockDate("fernando", "6077");
 
     return 0;
 }
---
+++ ui/Gui.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Juan Diego on 11/10/2022.
+//
+
+#include "Gui.h"
